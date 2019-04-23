64110c087d2de403d45e62f72d60ac488f988411
Artur Kuznetsov <artkuznetsov191@gmail.com>
2019-04-23 14:28:51 +0200

    master/feat(page): add title and drop menu

--- src/app/pages/social-tools-page/social-tools-page.component.ts
+++ src/app/pages/social-tools-page/social-tools-page.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-social-tools-page',
   templateUrl: './social-tools-page.component.html',
-  styleUrls: ['./social-tools-page.component.scss']
+  styleUrls: ['./social-tools-page.component.scss'],
 })
 export class SocialToolsPageComponent implements OnInit {
+
+  isShowDropMenu = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  showDropMenu() {
+    if (this.isShowDropMenu) {
+      this.isShowDropMenu = false;
+    } else {
+      this.isShowDropMenu = true;
+    }
+  }
 }
